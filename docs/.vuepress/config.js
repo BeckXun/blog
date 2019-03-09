4508cd8219483adaccbde594751a0322ddd6be37
xunjianfeng <xunjianfeng@huobi.com>
2019-03-09 10:48:24 +0800

    feat: config update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,16 +1,38 @@
 module.exports = {
   title: "BeckXun's Blog",
-  description: 'Just playing around',
+  description: 'Stay hungry, Stay foolish',
   base: "/",
   dest: "./dist",
   head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }]
+    ['link', {
+      rel: 'icon',
+      href: '/favicon.ico'
+    }]
   ],
   locales: {
     '/': {
       lang: 'zh-CN',
       title: "BeckXun's Blog",
-      description: 'Just playing around'
+      description: 'Stay hungry, Stay foolish'
     }
   },
-}
+  themeConfig: {
+    nav: [{
+        text: 'Note',
+        link: '/note/'
+      },
+      {
+        text: 'Experience',
+        link: '/exp/'
+      },
+      {
+        text: 'About',
+        link: '/about'
+      },
+      {
+        text: 'GitHub',
+        link: 'https://github.com/JamesXun/blog'
+      },
+    ]
+  }
+};
