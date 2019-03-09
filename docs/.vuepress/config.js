7f0bb8537a8dfe7d16cc049d0d4cdc2b76e7c8f8
xunjianfeng <xunjianfeng@huobi.com>
2019-03-09 09:56:48 +0800

    feat: add favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,5 +3,14 @@ module.exports = {
   description: 'Just playing around',
   base: "/",
   dest: "./dist",
-  
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: "BeckXun's Blog",
+      description: 'Just playing around'
+    }
+  },
 }
